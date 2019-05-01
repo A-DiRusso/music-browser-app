aee31082fa4289f9d92ac251648f4edd7282cfa4
Anthony DiRusso <ossurid@gmail.com>
2019-05-01 19:32:03 -0400

    makes Songs.js a class

--- src/Songs.js
+++ src/Songs.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function Songs() {
-  return (
-    <div>
-      <h2>How to Disappear Completely</h2>
-    </div>
-  )
+export default class Songs extends React.Component {
+  constructor(props) {
+      super(props);
+      this.setState = {
+
+      }
+  }
+  render() {
+    return (
+        <div>
+          <h2>How to Disappear Completely</h2>
+        </div>
+      )
+    
+  }
 }
