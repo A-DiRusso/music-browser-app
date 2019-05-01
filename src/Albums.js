0b47754f5f8f5688471d231db67d508c4ad6e140
Anthony DiRusso <ossurid@gmail.com>
2019-05-01 18:33:38 -0400

    adds dictionary values for Albums.js

--- src/Albums.js
+++ src/Albums.js
@@ -1,9 +1,53 @@
 import React from 'react'
 
-export default function Albums() {
-  return (
-    <div>
-      <h2>Kid A</h2>
-    </div>
-  )
+export default class Albums extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          radiohead: {
+              albums: ['Kid A',
+                       'The Bends',
+                       'Ok Comupter'
+                      ]
+          },
+          Migos: {
+              albums: ['Y.R.N(mixtape)',
+                        'Young Rich Nation',
+                        'Culture'
+                      ]
+                    },
+                    Santigold: {
+                        albums: ['Santogold',
+                                 'Master of My Make-Believe',
+                                 '99¢',
+                                ]
+                              },
+                              KidCudi: {
+                                  albums: ['Man on the Moon: The End of the Day',
+                                           'Indicud',
+                                           'Passion, Pain & Demon Slayin\''
+                                          ] 
+                                        },
+                                        OutCast: {
+                                            albums: ['Southernplayalisticadillacmuzik',
+                                                     'ATLiens',
+                                                     'Aquemini'
+                                                  ]
+                                    
+                                              }
+                                    
+                                          }
+                                      }
+    render() {
+    return (
+        <div>
+            <h2>Kid A</h2>
+        </div>
+        )
+    
+    }  
 }
+                    
+                              
+
+          
