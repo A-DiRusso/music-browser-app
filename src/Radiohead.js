e51056e2b18d063f8d8b1da6b00013b1812ec404
Anthony DiRusso <ossurid@gmail.com>
2019-05-02 13:04:27 -0400

    Radiohead.js can now render an album and a correct song list

--- src/Radiohead.js
+++ src/Radiohead.js
@@ -5,20 +5,37 @@ class Radiohead extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            albums: {
+            albums: [{
                 name: 'Kid A',
                 songs: ['How to Disappear Completely', 'Motion Picture Soundtrack'],
 
-            }
+            },
+            {
+                name: 'The Bends',
+                songs: [],
+            },
+            {
+                name: 'Ok Computer',
+                songs: [],
+            },
+            ]
         }
     }
     render() {
+        let albumsArray = Object.values(this.state.albums[0]);
+        console.log(albumsArray)
+        let songsArray = albumsArray.splice(1);
+        let albumArray = albumsArray.pop();
+        let songSpread = [...songsArray];
+        console.log(albumArray);
+        console.log('songsArray', songsArray);
       return (
         <div>
             <h3>Radiohead</h3>
+            <h4>{albumArray}</h4>
             <ul>
-                
-                    {this.state.albums.songs.map((song, i ) => (<li key={i}>{this.state.albums.name} : {song}</li>))}
+                    
+                    {songsArray[0].map((song, i) => <li key={i}>{song}</li>)}
                 
             </ul>
             <Link to="/bands">Back to Bands</Link>
@@ -29,3 +46,5 @@ class Radiohead extends React.Component {
 
 
 export default Radiohead;
+
+
