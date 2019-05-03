9fce21ee5915c43a573a9b055e7e025b595342b8
Anthony DiRusso <ossurid@gmail.com>
2019-05-03 08:51:46 -0400

    fixes kidcudi's renders

--- src/KidCudi.js
+++ src/KidCudi.js
@@ -13,12 +13,17 @@ export default class KidCudi extends Component {
         }
     }  
     render() {
+        // const albumName = Object.values(this.state.albums.name)
+        const albumName = Object.values(this.state.albums.name);
+        // const songsArray = albumsArray.splice(1);
+        // console.log(songsArray);
+        const kidCudiSongs = this.state.albums.songs.map((song, i) => (<li key={i}>{song}</li>))
+    
       return (
           <div>
               <h3>Kid Cudi</h3>
-              <ul>
-                  {this.state.albums.songs.map((song, i) => (<li key={i}>{this.state.albums.name} : {song}</li>))}
-              </ul>
+              <h4>{albumName}</h4>
+              <ul>{kidCudiSongs}</ul>
               <Link to="/bands">Back to Bands</Link>
           </div>
       )
